754b6beba9422e2bc5cf02a5bf6676686e130739
AndreyAD <arakelyants.andrey@yandex.ru>
2021-06-06 17:08:21 +0300

    Complete the exercise 4.6

--- ch4/compress_spaces/main.go
+++ ch4/compress_spaces/main.go
@@ -7,29 +7,25 @@ import (
 )
 
 func compressSpaces(slice []byte) []byte {
+	spaceBytes := []byte(" ")
+	spaceByteNumber := len(spaceBytes)
 	firstRune, size := utf8.DecodeRune(slice)
 	for index := size; index < len(slice); {
 		decodedRune, size := utf8.DecodeRune(slice[index:])
-		fmt.Printf("current rune '%c'\t previous rune '%c'", decodedRune, firstRune)
 		if unicode.IsSpace(firstRune) && firstRune == decodedRune {
-			fmt.Printf("delete symbol '%c'\n", decodedRune)
-			copy(slice[index:], slice[index + size:])
+			copy(slice[index - size:], spaceBytes[:])
+			copy(slice[index - size + spaceByteNumber:], slice[index + size:])
 			slice = slice[:len(slice) - size]
-			// index -= size
-			fmt.Println(index, string(slice))
-			continue
 		} else {
 			firstRune = decodedRune
 			index += size
 		}
-		fmt.Println(index, string(slice))
 	}
 	return slice
 }
 
 func main() {
-	// testString := "Go is a   programming\t\tlanguage\n\n"
-	testString := "Test\t\ttest"
+	testString := "Go is a   programming\t\tlanguage\n\n"
 	testSlice := []byte(testString)
 	compressedSlice := compressSpaces(testSlice)
 	fmt.Println(string(compressedSlice))
