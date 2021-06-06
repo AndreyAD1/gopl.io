710b5ccadc4d6f977b2cebadbfd205aa1320d3c9
AndreyAD <arakelyants.andrey@yandex.ru>
2021-06-06 16:32:07 +0300

    Fix the repeated spaces

--- ch4/compress_spaces/main.go
+++ ch4/compress_spaces/main.go
@@ -7,20 +7,6 @@ import (
 )
 
 func compressSpaces(slice []byte) []byte {
-	for index := 0; index < len(slice); {
-		spaceSlice := []byte(" ")
-		_, spaceSize := utf8.DecodeRune(spaceSlice)
-		decodedRune, size := utf8.DecodeRune(slice)
-		if unicode.IsSpace(decodedRune) {
-			copy(slice[index:], spaceSlice)
-			copy(slice[index + spaceSize:], slice[index + size:])
-			slice = slice[:len(slice) - size]
-			index -= size + spaceSize
-		} else {
-			index += size
-		}
-	}
-
 	firstRune, size := utf8.DecodeRune(slice)
 	for index := size; index < len(slice); {
 		decodedRune, size := utf8.DecodeRune(slice[index:])
@@ -29,19 +15,21 @@ func compressSpaces(slice []byte) []byte {
 			fmt.Printf("delete symbol '%c'\n", decodedRune)
 			copy(slice[index:], slice[index + size:])
 			slice = slice[:len(slice) - size]
-			index -= size
+			// index -= size
+			fmt.Println(index, string(slice))
 			continue
 		} else {
 			firstRune = decodedRune
 			index += size
 		}
-		fmt.Println(string(slice))
+		fmt.Println(index, string(slice))
 	}
 	return slice
 }
 
 func main() {
-	testString := "Go is a   programming \t\t   language\n\n"
+	testString := "Go is a   programming\t\tlanguage\n\n"
+	// testString := "Test  test"
 	testSlice := []byte(testString)
 	compressedSlice := compressSpaces(testSlice)
 	fmt.Println(string(compressedSlice))
