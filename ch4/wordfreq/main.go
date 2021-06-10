2a2214b7a65a8b2ee20e617506f46f37025942e1
AndreyAD <arakelyants.andrey@yandex.ru>
2021-06-10 23:46:43 +0300

    Make the script draft

--- ch4/wordfreq/main.go
+++ ch4/wordfreq/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -9,5 +12,12 @@ func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Error. The script needs a file path as an input argument")
 		return
+	}
+	fileBytes, err := ioutil.ReadFile(os.Args[1])
+	fileReader := bytes.NewReader(fileBytes)
+	fileScanner := bufio.NewScanner(fileReader)
+	fileScanner.Split(bufio.ScanWords)
+	for fileScanner.Scan() {
+		word := fileScanner.Text()
 	}
 }
