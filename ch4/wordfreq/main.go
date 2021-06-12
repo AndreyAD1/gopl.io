49139fe00c232ab0c2ec8aae1c2014e7cc984cbc
AndreyAD <arakelyants.andrey@yandex.ru>
2021-06-12 20:28:52 +0300

    Count and output the number of every word contained in a text

--- ch4/wordfreq/main.go
+++ ch4/wordfreq/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -14,10 +16,35 @@ func main() {
 		return
 	}
 	fileBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Printf("Can not read the file %s", os.Args[1])
+		return
+	}
 	fileReader := bytes.NewReader(fileBytes)
 	fileScanner := bufio.NewScanner(fileReader)
 	fileScanner.Split(bufio.ScanWords)
+	var maxWordLength int
+	wordInfo := make(map[string][2]int)
 	for fileScanner.Scan() {
 		word := fileScanner.Text()
+		lowerWord := strings.ToLower(word)
+		wordLenght := utf8.RuneCountInString(lowerWord)
+		if wordLenght > maxWordLength {
+			maxWordLength = wordLenght
+		}
+		wordNumber := wordInfo[lowerWord][0]
+		wordInfo[lowerWord] = [2]int{wordNumber + 1, wordLenght}
+	}
+	if fileScanner.Err() != nil {
+		fmt.Println("Error occured while scanner was reading the file")
+		return
+	}
+	for word, wordInfo := range wordInfo {
+		wordNumber, wordLength := wordInfo[0], wordInfo[1]
+		additionalRuneNumber := maxWordLength - wordLength
+		for r := 0; r < additionalRuneNumber; r++ {
+			word += " "
+		}
+		fmt.Printf("%s\t%d\n", word, wordNumber)
 	}
 }
