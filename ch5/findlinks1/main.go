2a708c4fefbb21718d74b39f245bbe0bf1321f16
Andrey Arakelyants <adarakelyants@domclick.ru>
2021-07-02 18:23:09 +0300

    Complete an exersice 5.4

--- ch5/findlinks1/main.go
+++ ch5/findlinks1/main.go
@@ -15,8 +15,11 @@ import (
 	"golang.org/x/net/html"
 )
 
+var hiddenNodes = map[string]bool{"script": true, "style": true}
+var linkTags = map[string]bool{"a": true, "img": true}
+
 func main() {
-	reader := strings.NewReader("<head>\n    <title>Example Domain</title>\n\n    <meta charset=\"utf-8\">\n    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <style type=\"text/css\">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n<style>@media print {#ghostery-tracker-tally {display:none !important}}</style></head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href=\"https://www.iana.org/domains/example\">More information...<a href=\"https://www.iana.org/domains/example2\"></a></p>\n</div>\n\n\n</body>")
+	reader := strings.NewReader("<head>\n    <title>Example Domain</title>\n\n    <meta charset=\"utf-8\">\n    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <style type=\"text/css\">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n<style>@media print {#ghostery-tracker-tally {display:none !important}}</style></head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href=\"https://www.iana.org/domains/example\">More information  <img src=\"img_chania.jpg\" alt=\"Flowers in Chania\"> ...<a href=\"https://www.iana.org/domains/example2\"></a></p>\n</div>\n\n\n</body>")
 	doc, err := html.Parse(reader)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
@@ -37,9 +40,10 @@ func main() {
 //!+visit
 // visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
-	if n.Type == html.ElementNode && n.Data == "a" {
+	isLink := linkTags[n.Data]
+	if n.Type == html.ElementNode && isLink {
 		for _, a := range n.Attr {
-			if a.Key == "href" {
+			if a.Key == "href" || a.Key == "src" {
 				links = append(links, a.Val)
 			}
 		}
@@ -57,7 +61,6 @@ func getTextContent(text []string, n *html.Node) []string {
 			text = append(text, n.Data)
 		}
 	}
-	hiddenNodes := map[string]bool{"script": true, "style": true}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		if hiddenNodes[n.Data] != true {
 			text = getTextContent(text, c)
