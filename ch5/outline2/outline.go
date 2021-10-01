b67f988fde31637b4436205e6c9b0e5c5c8465b2
AndreyAD <arakelyants.andrey@yandex.ru>
2021-10-02 00:38:14 +0300

    Ex. 7.4. Complete

--- ch5/outline2/outline.go
+++ ch5/outline2/outline.go
@@ -17,6 +17,8 @@ import (
 	"golang.org/x/net/html"
 )
 
+const exampleHTML = "<head>\n    <title>Example Domain</title>\n\n    <meta charset=\"utf-8\">\n    <meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n    <style type=\"text/css\">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n<style>@media print {#ghostery-tracker-tally {display:none !important}}</style></head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href=\"https://www.iana.org/domains/example\">More information...<a href=\"https://www.iana.org/domains/example2\"></a></p>\n</div>\n\n\n</body>"
+
 type StringReader string
 
 func (s StringReader) Read(p []byte) (int, error) {
@@ -28,7 +30,7 @@ func (s StringReader) Read(p []byte) (int, error) {
 	for {
 		run, runeSize := utf8.DecodeRuneInString(string(s))
 		if run == utf8.RuneError {
-			return readByteNumber, nil
+			return readByteNumber, io.EOF
 		}
 		readByteNumber += runeSize
 		if len(p) < readByteNumber {
@@ -59,11 +61,13 @@ func main() {
 				resp.Body.Close()
 			}
 		case "string":
-			stringReader := StringReader(os.Args[2])
+			stringReader := StringReader(exampleHTML)
 			err := outline(stringReader)
 			if err != nil {
 				fmt.Println(err)
 			}
+		default:
+			fmt.Println(commandError)
 	}
 
 }
