b18eac8da32ddb26c2a2b3a4fc6891bff1868bbd
AndreyAD <arakelyants.andrey@yandex.ru>
2021-10-10 20:40:14 +0300

    Ex. 7.9. Complete. Make an HTTP server

--- ch7/tablesorting/main.go
+++ ch7/tablesorting/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
 	"sort"
 )
 
 type Racer struct {
-	Name string
-	BirthYear int
-	TitleNumber int
+	Name         string
+	BirthYear    int
+	TitleNumber  int
 	FerrariRacer bool
 }
 
 type SortingTable struct {
-	table []Racer
-	lessFuncs []func (v1, v2 Racer) bool
+	table     []Racer
+	lessFuncs []func(v1, v2 Racer) bool
 }
 
 func (t SortingTable) Len() int {
@@ -26,7 +29,7 @@ func (t SortingTable) Swap(i, j int) {
 }
 
 func (t SortingTable) Less(i, j int) bool {
-	for _, lessFunc := range(t.lessFuncs) {
+	for _, lessFunc := range t.lessFuncs {
 		switch {
 		case lessFunc(t.table[i], t.table[j]):
 			return true
@@ -37,18 +40,63 @@ func (t SortingTable) Less(i, j int) bool {
 	return false
 }
 
+var htmlTableTemplate = template.Must(template.New("htmlTable").Parse(`
+<h1>Racers</h1>
+<table>
+<tr style='text-align: left'>
+  <th><a href="http://127.0.0.1:8000?sortby=name">Name</a></th>
+  <th><a href="http://127.0.0.1:8000?sortby=birth_year">Birth Year</a></th>
+  <th><a href="http://127.0.0.1:8000?sortby=title_number">Championships</a></th>
+  <th><a href="http://127.0.0.1:8000?sortby=ferrari_racer">Ferrari Racer</a></th>
+</tr>
+{{range .}}
+<tr>
+  <td>{{.Name}}</a></td>
+  <td>{{.BirthYear}}</td>
+  <td>{{.TitleNumber}}</a></td>
+  <td>{{.FerrariRacer}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
+
+var Racers = []Racer{
+	{"Michael Schumacher", 1969, 7, true},
+	{"Lewis Hamilton", 1985, 7, false},
+	{"Ayrton Senna", 1960, 3, false},
+	{"Juan Manuel Fangio", 1911, 5, true},
+}
+
+var funcsPerURLArgument = map[string]func(v1, v2 Racer) bool{
+	"name":          func(r1, r2 Racer) bool { return r1.Name < r2.Name },
+	"birth_year":    func(r1, r2 Racer) bool { return r1.BirthYear < r2.BirthYear },
+	"title_number":  func(r1, r2 Racer) bool { return r1.TitleNumber < r2.TitleNumber },
+	"ferrari_racer": func(r1, r2 Racer) bool { return r1.FerrariRacer && !r2.FerrariRacer },
+}
 
 func main() {
-	racers := []Racer{
-		{"Michael Schumacher", 1969, 7, true},
-		{"Lewis Hamilton", 1985, 7, false},
-		{"Ayrton Senna", 1960, 3, false},
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		getRacerTable(w, r)
 	}
-	nameIsLessFunc := func(r1, r2 Racer) bool {return r1.Name < r2.Name}
-	titleNumIsLess := func(r1, r2 Racer) bool {return r1.TitleNumber < r2.TitleNumber}
-	// birthYearIsLessFunc := func(r1, r2 Racer) bool {return r1.BirthYear < r2.BirthYear}
-	funcs := []func (v1, v2 Racer) bool {titleNumIsLess, nameIsLessFunc}
-	racerSortingTable := SortingTable{racers, funcs}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func getRacerTable(out io.Writer, request *http.Request) {
+	urlArgs := request.URL.Query()
+	sortArg := urlArgs.Get("sortby")
+	sortingFunc := funcsPerURLArgument[sortArg]
+
+	if sortingFunc == nil {
+		if err := htmlTableTemplate.Execute(out, Racers); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	sortingFuncs := []func(v1, v2 Racer) bool{sortingFunc}
+	racerSortingTable := SortingTable{Racers, sortingFuncs}
 	sort.Sort(racerSortingTable)
-	fmt.Println(racers)
+	if err := htmlTableTemplate.Execute(out, Racers); err != nil {
+		log.Fatal(err)
+	}
 }
