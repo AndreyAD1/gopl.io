d816e00063158b0e8c900ad90b30ff4200495251
AndreyAD <arakelyants.andrey@yandex.ru>
2021-10-17 18:28:25 +0300

    Ex. 7.16. Make the main page draft

---
+++ ch7/web_expression_evaluator/html_templates.go
@@ -0,0 +1,29 @@
+package main
+
+var MainPage = `<!DOCTYPE html>
+<html>
+<head>
+<title>Web Calculator</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>`
+
+var ErrorPage = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web Calculator/title>
+</head>
+<body>
+
+<h1>Internal Server Error</h1>
+<p>Something went wrong.</p>
+
+</body>
+</html>
+`
--- ch7/web_expression_evaluator/main.go
+++ ch7/web_expression_evaluator/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func getMainPage(w http.ResponseWriter, r *http.Request) {}
+
+func getMainPage(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Receive the request: %s %s", r.URL, r.Method)
+	w.Header().Set("Content-Type", "text/html")
+	_, err := fmt.Fprint(w, MainPage)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, ErrorPage)
+	}
+}
 
 func getExpressionResult(w http.ResponseWriter, r *http.Request) {}
 
