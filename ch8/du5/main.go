ea8970db8bbf6869bb03c8c986bb6c839927240f
AndreyAD <arakelyants.andrey@yandex.ru>
2022-01-06 22:01:09 +0300

    Ex. 8.9. Complete

--- ch8/du5/main.go
+++ ch8/du5/main.go
@@ -1,14 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 250.
-
-// The du3 command computes the disk usage of the files in a directory.
 package main
-
-// The du3 variant traverses all directories in parallel.
-// It uses a concurrency-limiting counting semaphore
-// to avoid opening too many files at once.
 
 import (
 	"flag"
@@ -29,19 +19,11 @@ type directoryInfo struct {
 
 //!+
 func main() {
-	// ...determine roots...
-
-	//!-
 	flag.Parse()
-
-	// Determine the initial directories.
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
-
-	//!+
-	// Traverse each root of the file tree in parallel.
 	fileSizes := make(chan int64)
 	dirSizes := make(chan directoryInfo)
 	subDirSizeChannel := make(chan int64)
@@ -51,13 +33,11 @@ func main() {
 		go walkDir(root, &n, fileSizes, dirSizes, subDirSizeChannel)
 	}
 	go func() {
+		<-subDirSizeChannel
 		n.Wait()
 		close(fileSizes)
 		close(dirSizes)
 	}()
-	//!-
-
-	// Print the results periodically.
 	var tick <-chan time.Time
 	if *vFlag {
 		tick = time.Tick(500 * time.Millisecond)
@@ -68,7 +48,7 @@ loop:
 		select {
 		case size, ok := <-fileSizes:
 			if !ok {
-				break loop // fileSizes was closed
+				break loop
 			}
 			nfiles++
 			nbytes += size
@@ -79,12 +59,8 @@ loop:
 		}
 	}
 
-	printDiskUsage(nfiles, nbytes) // final totals
-	//!+
-	// ...select loop...
+	printDiskUsage(nfiles, nbytes)
 }
-
-//!-
 
 func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
@@ -126,10 +102,6 @@ func walkDir(
 			totalSize += fileSize
 		}
 	}
-	// TODO make a separate channel for each walkDir.
-	// Send a current directory size to this channel.
-	// Receive a directory size from every channel and calculate
-	// the size of child directories.
 	for _, channel := range subDirSizeChannels {
 		subDirSize := <- channel
 		totalSize += subDirSize
@@ -138,19 +110,11 @@ func walkDir(
 	dirSizes <- directoryInfo{dir, totalSize}
 }
 
-//!-walkDir
-
-//!+sema
-// sema is a counting semaphore for limiting concurrency in dirents.
 var sema = make(chan struct{}, 20)
 
-// dirents returns the entries of directory dir.
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}        // acquire token
-	defer func() { <-sema }() // release token
-	// ...
-	//!-sema
-
+	sema <- struct{}{}
+	defer func() { <-sema }()
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du: %v\n", err)
