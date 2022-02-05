50bfe99b3b4f198d2811ed7fb2015143bd288894
AndreyAD <arakelyants.andrey@yandex.ru>
2022-02-05 19:37:30 +0300

    Ex. 9.1. Fix tha package name

--- ch9/bank1/bank.go
+++ ch9/bank1/bank.go
@@ -5,7 +5,7 @@
 //!+
 
 // Package bank provides a concurrency-safe bank with one account.
-package main
+package bank
 
 var deposits = make(chan int) // send amount to deposit
 var balances = make(chan int) // receive balance
