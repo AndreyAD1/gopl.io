1b8697578e9205bac7b026ba3705d38e1b5e13c9
Andrey Arakelyants <adarakelyants@domclick.ru>
2022-02-18 18:42:52 +0300

    Ex. 9.4. Complete

--- ch9/conveyer/main.go
+++ ch9/conveyer/main.go
@@ -7,8 +7,26 @@ import (
 	"time"
 )
 
-func runConveyer(goroutineNumber int) {
+func runConveyerStage(inputChannel, outputChannel chan struct{}) {
+	structure := <-inputChannel
+	outputChannel<- structure
+}
 
+func runConveyer(goroutineNumber int) time.Duration {
+	firstInputChannel := make(chan struct{})
+	var inputChannel, outputChannel chan struct{}
+	for i := 0; i < goroutineNumber; i++ {
+		inputChannel = outputChannel
+		if i == 0 {
+			inputChannel = firstInputChannel
+		}
+		outputChannel = make(chan struct{})
+		go runConveyerStage(inputChannel, outputChannel)
+	}
+	start := time.Now()
+	firstInputChannel<- struct{}{}
+	<-outputChannel
+	return time.Since(start)
 }
 
 func main() {
@@ -21,11 +39,12 @@ func main() {
 	inputArgument := os.Args[1]
 	goroutineNumber, err := strconv.Atoi(inputArgument)
 	if err != nil {
-		fmt.Printf("The argument '%s' is not an integer\n", inputArgument)
+		fmt.Printf("ERROR. The argument '%s' is not an integer\n", inputArgument)
 	}
-	start := time.Now()
-	runConveyer(goroutineNumber)
-	workDuration := time.Since(start)
+	if goroutineNumber < 1 {
+		fmt.Println("ERROR. The goroutine number is less than 1.")
+	}
+	workDuration := runConveyer(goroutineNumber)
 	fmt.Printf(
 		"It took %s to pass a conveyer of %d goroutines\n",
 		workDuration,
